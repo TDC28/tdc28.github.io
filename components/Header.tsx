25b348a16788dffdfb9c452c750bd2e71a4e30a8
TDC28 <a06x03g07t01@protonmail.com>
2024-08-30 20:34:00 -0300

    Github actions

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
 
   return (
     <div className="flex justify-center items-center relative top-3">
-      <nav className="flex gap-1 p-0.5 rounded-full bg-white/10 backdrop-blur">
+      <nav className="flex gap-1 p-0.5 rounded-full bg-white/10">
         <Link
           className={`nav-item ${isActive("/") ? "bg-white text-gray-900 hover:bg-white hover:text-gray-900" : ""}`}
           href="/"
