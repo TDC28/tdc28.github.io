298147856b418b60f1cced00297f49197757ff86
TDC28 <a06x03g07t01@protonmail.com>
2024-08-30 16:58:44 -0300

    Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
+import { twMerge } from "tailwind-merge";
+import { Header } from "@/components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +23,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans",
+        )}
+      >
+        {" "}
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const Header = () => {
+  const pathName = usePathname();
+
+  const isActive = (path: string): boolean => path === pathName;
+
+  return (
+    <div className="flex justify-center items-center relative top-3">
+      <nav className="flex gap-1 p-0.5 rounded-full bg-white/10 backdrop-blur">
+        <Link
+          className={`nav-item ${isActive("/") ? "bg-white text-gray-900 hover:bg-white hover:text-gray-900" : ""}`}
+          href="/"
+        >
+          Home
+        </Link>
+        <Link
+          className={`nav-item ${isActive("/projects") ? "bg-white text-gray-900 hover:bg-white hover:text-gray-900" : ""}`}
+          href="/projects"
+        >
+          Projects
+        </Link>
+        <Link
+          className={`nav-item ${isActive("/about") ? "bg-white text-gray-900 hover:bg-white hover:text-gray-900" : ""}`}
+          href="/about"
+        >
+          About
+        </Link>
+        <Link
+          className={`nav-item ${isActive("/contact") ? "bg-white text-gray-900 hover:bg-white hover:text-gray-900" : ""}`}
+          href="/contact"
+        >
+          Contact
+        </Link>
+      </nav>
+    </div>
+  );
+};
